b4d71578ed2eb8c07ebe91685ab27f185e3aaae2
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-21 08:51:09 +0300

    creates first templates

--- netflix/views.py
+++ netflix/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    title = "Netflix Clone Application"
+
+    return render(request,'index.html',{"title":title})
